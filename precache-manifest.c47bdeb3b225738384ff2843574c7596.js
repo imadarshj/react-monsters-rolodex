12e59e0c02d7ae01a7f140be78e57b5694a2f0d2
Adarsh Jaiswal <geniusadarsh3@gmail.com>
2020-02-09 16:56:05 +0530

    Updates

---
+++ precache-manifest.c47bdeb3b225738384ff2843574c7596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4a0f588d144e8d2dd434919941dc5d",
+    "url": "/imadarshj/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "458fc03a263f5caa841e",
+    "url": "/imadarshj/react-monsters-rolodex/static/css/main.73964f1a.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/imadarshj/react-monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/imadarshj/react-monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "458fc03a263f5caa841e",
+    "url": "/imadarshj/react-monsters-rolodex/static/js/main.d8bc0dcc.chunk.js"
+  },
+  {
+    "revision": "4eef6e8e7b1779a3ad5c",
+    "url": "/imadarshj/react-monsters-rolodex/static/js/runtime-main.b70a4705.js"
+  }
+]);
